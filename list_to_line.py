6956261f2007d3583cfe34340302de72d41b21d8
Tom <thomas.dell@outlook.com.au>
2018-10-23 09:33:34 +1100

    Update list_to_line.py

--- list_to_line.py
+++ list_to_line.py
@@ -1,3 +1,23 @@
+"""
+This script will convert a list of
+
+1
+2
+3
+
+to 1, 2, 3
+
+
+Instructions
+
+paste your list into input.txt
+save input.txt
+run listtoline.py
+output.txt will open
+replace the last "," with a "."
+"""
+
+
 import os
 
 i = open("input.txt", "r")
